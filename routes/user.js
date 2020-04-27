dbf0e8537943cf71a7c281b30179ad0d741177c9
weblixin <weblixin@163.com>
2020-04-27 19:13:12 +0800

    user路由修改

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,34 @@
 const router = require('koa-router')();
 const UserModel = require('../model/User.js');
-
+const md5 = require('../util/encryptByMD5');
+const salt = "QE4CwVWGy1lBBIW5uoYFsZEwfyI7ScuU";
+const routers = require('../conf/router');
+console.log(routers["0"])
 router.prefix('/user');
 
+router.post('/login', async (ctx, next) => {
+    let { username, password } = ctx.request.body;
+    let result = await UserModel.findOne({username});
+    if(result._id && md5(md5(result.password + salt)) === password) {
+        result.loginAt = Date.now();
+        await result.save();
+        result = result.toObject();
+        const {role} = result;
+        delete result.password;
+        delete result.role;
+        ctx.body = {code: 1, msg: '登录成功', data: result, routers: routers[role.toString()]}
+    } else {
+        ctx.body = {code: -1, msg: '用户名或密码不正确'}
+    }
+});
+
 router.post('/', async (ctx, next) => {
-    let { username, password, role, remarks, nick_name } = ctx.request.body;
+    let { username, password, role, remarks, nickName } = ctx.request.body;
     let result = await UserModel.find({username});
     if(result.length > 0) {
         ctx.body = {code: 2, msg: "该账户名已存在，请检查输入是否有误"}
     } else {
-        let data = await UserModel.create({ username, nick_name, password, role, remarks, loginAt: Date.now() });
+        let data = await UserModel.create({ username, nickName, password, role, remarks});
         if(data) {
             ctx.body = {code: 1, msg: '账户创建成功', data}
         } else {
@@ -78,24 +97,6 @@ router.delete('/', async (ctx, next) => {
     } else {
         ctx.response.status = 400;
         ctx.body = {code: -1, msg: "删除失败"}
-    }
-});
-
-router.post('/login', async (ctx, next) => {
-    let { username, password } = ctx.request.body;
-    if(username && password) {
-        let result = await UserModel.find({username, password});
-        if(result.length > 0) {
-            result[0].loginAt = Date.now();
-            await result[0].save();
-            ctx.body = {code: 1, msg: '登录成功', data: result}
-        } else {
-            ctx.response.status = 400;
-            ctx.body = {code: -1, msg: '用户名或密码不正确'}
-        }
-    } else {
-        ctx.response.status = 400;
-        ctx.body = {code: -1, msg: '用户名或密码不能为空'}
     }
 });
 
