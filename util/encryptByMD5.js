0ffe7693ddd94119645382121c61a473f37a5b31
weblixin <weblixin@163.com>
2020-04-27 19:13:34 +0800

    md5加密方法

---
+++ util/encryptByMD5.js
@@ -0,0 +1,8 @@
+const crypto = require("crypto");
+
+module.exports = function md5(str) {
+    let md5 = crypto.createHash('md5');
+    md5.update(str, "utf8");
+    str = md5.digest('hex');
+    return str
+};
