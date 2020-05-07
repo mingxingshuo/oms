aad120c3a578a6652b1601166c0367c39cfd1c45
weblixin <weblixin@163.com>
2020-05-07 10:46:26 +0800

    更换渲染方式

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,23 @@ const fs = require('fs');
 
 router.prefix('/');
 
-// router.get('/', async function(ctx, next) {
-//     const htmlFile = await readHtmlFile(path.join(__dirname, '../public', 'index.html'));
-//     ctx.type = 'html';
-//     ctx.body = htmlFile;
-// });
-//
-// router.get('/admin/*', async function(ctx, next) {
-//     const htmlFile = await readHtmlFile(path.join(__dirname, '../build', 'index.html'));
-//     ctx.type = 'html';
-//     ctx.body = htmlFile;
-// });
-//
-// router.get('/admin', async function(ctx, next) {
-//     const htmlFile = await readHtmlFile(path.join(__dirname, '../build', 'index.html'));
-//     ctx.type = 'html';
-//     ctx.body = htmlFile;
-// });
+router.get('/', async function(ctx, next) {
+    const htmlFile = await readHtmlFile(path.join(__dirname, '../public', 'index.html'));
+    ctx.type = 'html';
+    ctx.body = htmlFile;
+});
+
+router.get('/admin/*', async function(ctx, next) {
+    const htmlFile = await readHtmlFile(path.join(__dirname, '../build', 'index.html'));
+    ctx.type = 'html';
+    ctx.body = htmlFile;
+});
+
+router.get('/admin', async function(ctx, next) {
+    const htmlFile = await readHtmlFile(path.join(__dirname, '../build', 'index.html'));
+    ctx.type = 'html';
+    ctx.body = htmlFile;
+});
 
 function readHtmlFile(filePath) {
     return new Promise(function(resolve, reject){
