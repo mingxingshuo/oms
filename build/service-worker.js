54907ac8a028cef58b6e5ede4b9e2571473b1c16
weblixin <weblixin@163.com>
2020-05-09 11:37:33 +0800

    订单号随机生成

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.414a76b4e8f38f7238f89844a305e318.js"
+  "/precache-manifest.efe9f566eb5e77beaed78648a7af5277.js"
 );
 
 self.addEventListener('message', (event) => {
