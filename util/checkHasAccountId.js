340ab85f3677f01d8d571642ffed03213635b7cc
weblixin <weblixin@163.com>
2020-06-01 18:03:07 +0800

    ok

--- util/checkHasAccountId.js
+++ util/checkHasAccountId.js
@@ -2,7 +2,7 @@ const jwt = require("./jsonwebtoken");
 
 async function checkHasAccountId(ctx, next) {
     let {token} = ctx.request.header;
-    if(ctx.request.url === ("/user/login")) {
+    if(ctx.request.url === ("/user/login" || "/pay/upload")) {
         await next()
     } else if (token) {
         await jwt.checkToken(token)
