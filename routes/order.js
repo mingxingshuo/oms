44e31b63e019ab9e1b4958de61550c61f8549364
efdssdf <efdssdf@gmail.com>
2020-06-08 14:37:49 +0800

    'log'

--- routes/order.js
+++ routes/order.js
@@ -203,11 +203,11 @@ router.get('/review', async function (ctx, next) {
 
 router.post('/submit', async function (ctx, next) {
     let {orderids} = ctx.request.body
-    console.log(orderids,'-------------------orderids')
+    console.log(orderids, '-------------------orderids')
     for (let orderid of orderids) {
-        console.log(orderid,'-------------------orderid')
+        console.log(orderid, '-------------------orderid')
         let order = await ReviewOrderModel.findOne({orderid: orderid})
-        console.log(order,'-------------------order')
+        console.log(order, '-------------------order')
         let url = "https://bsp-oisp.sf-express.com/bsp-oisp/sfexpressService"
         let xml = {
             Request: {
@@ -542,7 +542,7 @@ function req(data) {
         let url = "https://bsp-oisp.sf-express.com/bsp-oisp/sfexpressService"
         request.post(url, data, function (err, res, body) {
             parser.parseString(body, function (err1, result) {
-                console.log(result,'-----------------------result')
+                console.log(err1, result, '-----------------------result')
                 if (result.Response.ERROR) {
                     resolve({type: 1, data: result.Response.ERROR[0]})
                 } else {
